3ca6fa7842f24767b80eb273284e44143b48e646
Slard <wiltshire.slade@gmail.com>
2022-04-14 15:19:17 -0400

    Imported Jen's code

--- src/main/java/com/example/converter340/MainActivity.java
+++ src/main/java/com/example/converter340/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.converter340;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String TAG = "CONVERTIT_MAIN";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.currency_btn);
+        button.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        int eventSourceId = view.getId();
+        Log.d(TAG, String.format("event source id: %s", eventSourceId));
+
+        if (eventSourceId == R.id.currency_btn) {
+            handleCurrencyButtonClick();
+        }
+    }
+
+    private void handleCurrencyButtonClick() {
+        Intent intent = new Intent(this, Currency.class);
+        startActivity(intent);
+    }
+
 }
