34ef3139e56c8bc4596ea8bdd6e81a5fe1d5224c
Slard <wiltshire.slade@gmail.com>
2022-04-14 15:22:41 -0400

    adding jen's code
    adding jen's code

---
+++ src/main/java/com/example/converter340/Currency.java
@@ -0,0 +1,53 @@
+package com.example.converter340;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Currency extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String TAG = "CONVERTIT_CURRENCY";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.currency);
+
+        Spinner spinnerInput;
+        spinnerInput = findViewById(R.id.currency_spinner_input);
+        ArrayAdapter<CharSequence> adapterIn = ArrayAdapter.createFromResource(this,
+                R.array.currencySpinnerValues,
+                android.R.layout.simple_spinner_item);
+        adapterIn.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerInput.setAdapter(adapterIn);
+
+        Spinner spinnerOutput;
+        spinnerOutput = findViewById(R.id.currency_spinner_output);
+        ArrayAdapter<CharSequence> adapterOut = ArrayAdapter.createFromResource(this,
+                R.array.currencySpinnerValues,
+                android.R.layout.simple_spinner_item);
+        adapterOut.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerOutput.setAdapter(adapterOut);
+
+        findViewById(R.id.back_btn).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int eventSourceId = view.getId();
+        Log.d(TAG, String.format("Clicked on: %s", eventSourceId));
+
+        if (eventSourceId == R.id.back_btn) {
+            finish();
+        }
+        else
+            Log.d(TAG, String.format("Unknown click event source: %s", eventSourceId));
+    }
+
+}
